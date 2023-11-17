f491ddab00c2ee88bf995460413633cfc762a6a4
0xyshv <writetoyshv@gmail.com>
2023-11-17 22:40:00 +0530

    chore: CRUD operations with DWN

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,239 +8,113 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   const [web5, setWeb5] = useState(null);
   const [myDid, setMyDid] = useState(null);
-  const [activeRecipient, setActiveRecipient] = useState(null);
-
-  const [receivedDings, setReceivedDings] = useState([]);
-  const [sentDings, setSentDings] = useState([]);
-
-  const [noteValue, setNoteValue] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [recipientDid, setRecipientDid] = useState("");
-
-  const [didCopied, setDidCopied] = useState(false);
-  const [showNewChatInput, setShowNewChatInput] = useState(false);
-
-  const allDings = [...receivedDings, ...sentDings];
-
-  const sortedDings = allDings.sort(
-    (a, b) => new Date(a.timestampWritten) - new Date(b.timestampWritten)
-  );
-
-  const groupedDings = allDings.reduce((acc, ding) => {
-    const recipient = ding.sender === myDid ? ding.recipient : ding.sender;
-    if (!acc[recipient]) acc[recipient] = [];
-    acc[recipient].push(ding);
-    return acc;
-  }, {});
+  const [recordId, setRecordId] = useState();
 
   useEffect(() => {
     const initWeb5 = async () => {
       const { web5, did } = await Web5.connect();
       setWeb5(web5);
       setMyDid(did);
-
-      if (web5 && did) {
-        await configureProtocol(web5);
-        await fetchDings(web5, did);
-      }
     };
     initWeb5();
   }, []);
 
-  useEffect(() => {
-    if (!web5 || !myDid) return;
-    const intervalId = setInterval(async () => {
-      await fetchDings(web5, myDid);
-    }, 2000);
-
-    return () => clearInterval(intervalId);
-  }, [web5, myDid]);
-
-  const configureProtocol = async (web5) => {
-    const dingerProtocolDefinition = {
-      protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
-      published: true,
-      types: {
-        ding: {
-          schema: "https://blackgirlbytes.dev/ding",
-          dataFormats: ["application/json"],
-        },
+  async function createJsonRecord() {
+    const { record } = await web5.dwn.records.create({
+      data: {
+        content: "Hello Web5",
+        description: "Keep Building!",
       },
-      structure: {
-        ding: {
-          $actions: [
-            { who: "anyone", can: "write" },
-            { who: "author", of: "ding", can: "read" },
-            { who: "recipient", of: "ding", can: "read" },
-          ],
-        },
-      },
-    };
-
-    const { protocols, status: protocolStatus } =
-      await web5.dwn.protocols.query({
-        message: {
-          filter: {
-            protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
-          },
-        },
-      });
-
-    if (protocolStatus.code !== 200 || protocols.length === 0) {
-      const { protocolStatus } = await web5.dwn.protocols.configure({
-        message: {
-          definition: dingerProtocolDefinition,
-        },
-      });
-      console.log("Configure protocol status", protocolStatus);
-    }
-  };
-
-  const constructDing = () => {
-    const currentDate = new Date().toLocaleDateString();
-    const currentTime = new Date().toLocaleTimeString();
-    const ding = {
-      sender: myDid,
-      note: noteValue,
-      recipient: recipientDid,
-      timestampWritten: `${currentDate} ${currentTime}`,
-    };
-    return ding;
-  };
-
-  const writeToDwn = async (ding) => {
-    const { record } = await web5.dwn.records.write({
-      data: ding,
       message: {
-        protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
-        protocolPath: "ding",
-        schema: "https://blackgirlbytes.dev/ding",
-        recipient: recipientDid,
+        dataFormat: "application/json",
+        published: true,
       },
     });
-    return record;
-  };
 
-  const sendRecord = async (record) => {
-    return await record.send(recipientDid);
-  };
+    console.log("Record created:", record);
+    setRecordId(record._recordId);
+    console.log(record._recordId);
+  }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!noteValue.trim()) {
-      setErrorMessage("Please type a message before sending.");
-      return;
-    }
-
-    const ding = constructDing();
-    const record = await writeToDwn(ding);
-    const { status } = await sendRecord(record);
-
-    console.log("Send record status", status);
-
-    await fetchDings(web5, myDid);
-  };
-
-  const handleCopyDid = async () => {
-    if (myDid) {
-      try {
-        await navigator.clipboard.writeText(myDid);
-        setDidCopied(true);
-
-        setTimeout(() => {
-          setDidCopied(false);
-        }, 3000);
-      } catch (err) {
-        console.log("Failed to copy DID: " + err);
-      }
-    }
-  };
-
-  const fetchDings = async (web5, did) => {
-    const { records, status: recordStatus } = await web5.dwn.records.query({
+  async function queryRecords() {
+    const { records } = await web5.dwn.records.query({
+      from: did,
       message: {
         filter: {
-          protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
-          protocolPath: "ding",
+          schema: "https://schema.org/Playlist",
+          dataFormat: "application/json",
         },
-        dateSort: "createdAscending",
+      },
+    });
+  }
+
+  async function readRecord() {
+    // Reads the indicated record from the user's DWNs
+    let { record } = await web5.dwn.records.read({
+      message: {
+        filter: {
+          recordId: recordId,
+        },
       },
     });
 
-    try {
-      const results = await Promise.all(
-        records.map(async (record) => record.data.json())
-      );
+    // assuming the record has a text payload
+    const text = await record.data.json();
 
-      if (recordStatus.code == 200) {
-        const received = results.filter((result) => result?.recipient === did);
-        const sent = results.filter((result) => result?.sender === did);
-        setReceivedDings(received);
-        setSentDings(sent);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    console.log("Read Record:", text);
+  }
 
-  const handleStartNewChat = () => {
-    setActiveRecipient(null);
-    setShowNewChatInput(true);
-  };
+  async function updateRecord() {
+    // Get the record
+    const { record } = await web5.dwn.records.read({
+      message: {
+        filter: {
+          recordId: recordId,
+        },
+      },
+    });
 
-  const handleSetActiveRecipient = (recipient) => {
-    setRecipientDid(recipient);
-    setActiveRecipient(recipient);
-    setShowNewChatInput(false);
-  };
+    // Update the record
+    const { status } = await record.update({
+      data: {
+        content: "Hello Web6",
+        description: "Keep Building!",
+      },
+    });
+    console.log("Updated record:", status);
+  }
 
-  const handleConfirmNewChat = () => {
-    setActiveRecipient(recipientDid);
-    setActiveRecipient(recipientDid);
-    setShowNewChatInput(false);
-    if (!groupedDings[recipientDid]) {
-      groupedDings[recipientDid] = [];
-    }
-  };
+  async function deleteRecord() {
+    //Query records with plain text data format
+    const response = await web5.dwn.records.query({
+      message: {
+        filter: {
+          recordId: recordId,
+        },
+      },
+    });
 
+    // Grab the first indexed record
+    const record = response.records[0];
+
+    // Delete the record
+    const deleteResult = await record.delete();
+    console.log("Delete:", deleteResult);
+  }
   return (
     <div className="app-container">
-      {/* <header>
-        <h1>Dinger : {myDid}</h1>
-      </header> */}
-      <Button variant="outline">Button</Button>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      {/* <main>
-        <Sidebar
-          groupedDings={groupedDings}
-          activeRecipient={activeRecipient}
-          handleSetActiveRecipient={handleSetActiveRecipient}
-          handleCopyDid={handleCopyDid}
-          handleStartNewChat={handleStartNewChat}
-          showNewChatInput={showNewChatInput}
-          didCopied={didCopied}
-          handleConfirmNewChat={handleConfirmNewChat}
-          setRecipientDid={setRecipientDid}
-          recipientDid={recipientDid}
-        />
-        <section>
-          {activeRecipient ? (
-            <Chat
-              activeRecipient={activeRecipient}
-              sortedDings={sortedDings}
-              myDid={myDid}
-              handleSubmit={handleSubmit}
-              noteValue={noteValue}
-              setNoteValue={setNoteValue}
-              errorMessage={errorMessage}
-              setErrorMessage={setErrorMessage}
-            />
-          ) : (
-            <NoChatSelected />
-          )}
-        </section>
-      </main> */}
+      <Button variant="outline" onClick={createJsonRecord}>
+        Create Record
+      </Button>
+      <Button variant="outline" onClick={readRecord}>
+        Read Record
+      </Button>
+      <Button variant="outline" onClick={updateRecord}>
+        Update Record
+      </Button>
+      <Button variant="outline" onClick={deleteRecord}>
+        Delete Record
+      </Button>
     </div>
   );
 }
