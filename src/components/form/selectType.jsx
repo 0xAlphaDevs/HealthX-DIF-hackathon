118d482b858fff954773b1e97b3037895f9f1b49
0xyshv <writetoyshv@gmail.com>
2023-11-20 11:16:46 +0530

    feat: add animation

--- src/components/form/selectType.jsx
+++ src/components/form/selectType.jsx
@@ -6,8 +6,8 @@ export default function SelectType({ setType, setStep }) {
   // console.log(value);
 
   return (
-    <div className="flex flex-col mt-40 justify-center items-center translate-y-6">
-      <h1 className="text-5xl mb-8 font-semibold">
+    <div className="flex flex-col mt-40 justify-center items-center animate-in slide-in-from-bottom fade-in duration-700 ">
+      <h1 className="text-5xl mb-8 font-semibold ">
         Are you an individual or Organization?
       </h1>
       <ToggleGroup
