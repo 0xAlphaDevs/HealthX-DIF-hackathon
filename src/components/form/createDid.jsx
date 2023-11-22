6db03c186cc31b6323f7f7c860d5944e5859dcd5
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-11-22 09:55:52 +0530

    sync

--- src/components/form/createDid.jsx
+++ src/components/form/createDid.jsx
@@ -2,20 +2,40 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Loader from "../loader";
+import { Web5 } from "@web5/api";
 
 export default function CreateDid({ name }) {
   const router = useRouter();
+  const [web5, setWeb5] = useState(null);
+  const [myDid, setMyDid] = useState(null);
 
   const [text, setText] = useState(
     " You can now experience the power of web5!"
   );
   const [loading, setLoading] = useState(false);
-  function handleClick() {
-    // go to homepage
-    // router.push("/");
+
+  // create did using web5
+  async function generateDid() {
+    const { web5, did } = await Web5.connect();
+    setWeb5(web5);
+    setMyDid(did);
+    return did;
+  }
+
+  async function getDidDocument(did) {
+    const didDocument = await web5.did.resolve(did);
+    return didDocument;
+  }
+
+  async function handleClick() {
     setLoading(true);
     setText("Please wait while we create your DID");
+    const did = await generateDid();
+    console.log(did);
+    const didDocument = await getDidDocument(did);
+    console.log(didDocument);
   }
+
   return (
     <div className=" flex flex-col h-screen justify-center items-center">
       <h1 className="text-3xl font-bold">Welcome, {name} </h1>
