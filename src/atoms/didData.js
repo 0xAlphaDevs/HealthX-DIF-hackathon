39ce73fd0fe37da88dc726742a7902260c16ca1f
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-11-28 10:46:58 +0530

    sync changes

--- src/atoms/didData.js
+++ src/atoms/didData.js
@@ -1,8 +1,14 @@
+"use client";
 import { atom } from "recoil";
+
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 const didState = atom({
   key: "didState", // unique ID (with respect to other atoms/selectors)
   default: { did: "", name: "", year: "" }, // default value (aka initial value)
+  effects_UNSTABLE: [persistAtom],
 });
 
 export default didState;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { useRecoilState } from "recoil";
 import didState from "@/atoms/didData";
