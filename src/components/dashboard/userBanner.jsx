d6dd434b970514359394796808a8b749a57ac74c
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-11-30 09:40:03 +0530

    feat: add copy feature

--- src/components/dashboard/userBanner.jsx
+++ src/components/dashboard/userBanner.jsx
@@ -10,27 +10,90 @@ import {
 } from "@/components/ui/card";
 import { useRecoilState } from "recoil";
 import didState from "@/atoms/didData";
+import { CopyIcon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const UserBanner = () => {
   const [didData, setDidData] = useRecoilState(didState);
-  const formattedDid = `${didData.did.slice(8, 14)}...${didData.did.slice(-6)}`;
+  const formattedDid = `${didData.did.slice(8, 16)}...${didData.did.slice(-8)}`;
+
+  function handleCopyToClipboard() {
+    navigator.clipboard.writeText(didData.did);
+  }
   return (
-    <Card className="m-8 text-emerald-900 bg-emerald-50">
-      <CardHeader className="flex flex-row justify-between ">
-        <div className="">
-          <CardTitle>DID :</CardTitle>
-          <CardDescription>{formattedDid}</CardDescription>
-        </div>
-        <div>
-          <CardTitle>User Name:</CardTitle>
-          <CardDescription>{didData.name}</CardDescription>
-        </div>
-        <div>
-          <CardTitle>D.O.B. :</CardTitle>
-          <CardDescription>{didData.year}</CardDescription>
-        </div>
-      </CardHeader>
-    </Card>
+    <div className="grid grid-cols-2 gap-8 m-8">
+      <Card className=" text-emerald-900 bg-emerald-50">
+        <CardHeader className="flex flex-col justify-between ">
+          <div className="">
+            <CardTitle>DID</CardTitle>
+            <div className="flex">
+              <CardDescription>{formattedDid}</CardDescription>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div
+                      className="cursor-pointer ml-2 mr-4 h-8 text-md"
+                      onClick={handleCopyToClipboard}
+                    >
+                      <CopyIcon />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Copy DID</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          </div>
+          <div>
+            <CardTitle>User Name</CardTitle>
+            <CardDescription>{didData.name}</CardDescription>
+          </div>
+          <div>
+            <CardTitle>D.O.B.</CardTitle>
+            <CardDescription>{didData.year}</CardDescription>
+          </div>
+        </CardHeader>
+      </Card>
+      <Card className=" text-emerald-900 bg-emerald-50">
+        {/* <CardHeader className="flex flex-col justify-between ">
+          <div className="">
+            <CardTitle>DID</CardTitle>
+            <div className="flex">
+              <CardDescription>{formattedDid}</CardDescription>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div
+                      className="cursor-pointer ml-2 mr-4 h-8 text-md"
+                      onClick={handleCopyToClipboard}
+                    >
+                      <CopyIcon />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Copy DID</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          </div>
+          <div>
+            <CardTitle>User Name</CardTitle>
+            <CardDescription>{didData.name}</CardDescription>
+          </div>
+          <div>
+            <CardTitle>D.O.B.</CardTitle>
+            <CardDescription>{didData.year}</CardDescription>
+          </div>
+        </CardHeader> */}
+      </Card>
+    </div>
   );
 };
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,10 +4,22 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { useRecoilState } from "recoil";
 import didState from "@/atoms/didData";
+import { CopyIcon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const Navbar = () => {
   const [didData, setDidData] = useRecoilState(didState);
-  const formattedDid = `${didData.did.slice(8, 14)}...${didData.did.slice(-6)}`;
+  const formattedDid = `${didData.did.slice(8, 16)}...${didData.did.slice(-8)}`;
+
+  function handleCopyToClipboard() {
+    navigator.clipboard.writeText(didData.did);
+  }
+
   return (
     <div className="p-2 bg-emerald-800 flex justify-between">
       <div className="flex gap-2 items-center">
@@ -18,7 +30,24 @@ const Navbar = () => {
         <p className="font-bold text-emerald-100 text-xl">HealthX</p>
       </div>
 
-      <Badge>{formattedDid}</Badge>
+      <div>
+        <Badge className="h-8 text-md">{formattedDid}</Badge>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Badge
+                className="cursor-pointer ml-2 mr-4 h-8 text-md"
+                onClick={handleCopyToClipboard}
+              >
+                <CopyIcon />
+              </Badge>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Copy DID</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </div>
   );
 };
