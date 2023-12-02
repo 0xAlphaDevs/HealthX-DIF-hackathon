2d4fafdec57b924683a74173c2647ce6458a8196
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-02 18:33:31 +0530

    add endpoint

--- src/helpers/initWeb5.js
+++ src/helpers/initWeb5.js
@@ -2,9 +2,9 @@ import { Web5 } from "@web5/api";
 
 export const initWeb5 = async () => {
   const { web5, did } = await Web5.connect({
-    // techPreview: {
-    //   dwnEndpoints: ["https://healthx-dwn-server.onrender.com"],
-    // },
+    techPreview: {
+      dwnEndpoints: ["https://healthx-dwn-server.onrender.com"],
+    },
     sync: "5s",
   });
   return { web5, did };
