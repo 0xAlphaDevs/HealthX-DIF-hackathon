e8bfb2e7b1a43539ea65f7f904e6d3ba7135640d
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-03 08:03:21 +0530

    chore: add tech preview

--- src/helpers/initWeb5.js
+++ src/helpers/initWeb5.js
@@ -1,6 +1,10 @@
 import { Web5 } from "@web5/api";
 
 export const initWeb5 = async () => {
-  const { web5, did } = await Web5.connect({});
+  const { web5, did } = await Web5.connect({
+    techPreview: {
+      dwnEndpoints: ["https://healthx-dwn-server.onrender.com"],
+    },
+  });
   return { web5, did };
 };
