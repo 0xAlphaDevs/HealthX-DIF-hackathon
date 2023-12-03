bd777847df136b189a6142dfebf22802e62bc150
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-03 20:59:29 +0530

    chore: update protocol

--- src/helpers/initWeb5.js
+++ src/helpers/initWeb5.js
@@ -1,7 +1,7 @@
 import { Web5 } from "@web5/api";
 
 export const initWeb5 = async () => {
-  const { web5, did } = await Web5.connect({});
+  const { web5, did } = await Web5.connect();
   return { web5, did };
 };
 
