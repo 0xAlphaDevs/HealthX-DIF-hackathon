12f0148efc05f6d27058eae7008b858b807c5285
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-03 21:11:02 +0530

    chore: update options

--- src/helpers/initWeb5.js
+++ src/helpers/initWeb5.js
@@ -2,6 +2,9 @@ import { Web5 } from "@web5/api";
 
 export const initWeb5 = async () => {
   const { web5, did } = await Web5.connect({
+    techPreview: {
+      dwnEndpoints: ["https://dwn.tbddev.org/dwn3"],
+    },
     sync: "5s",
   });
   return { web5, did };
